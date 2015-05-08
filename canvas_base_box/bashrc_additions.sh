4bcc6819ca59d04a3ac8dbd936465c978144f865
Dan Collis-Puro <dan@collispuro.net>
2015-05-07 21:03:24 -0400

    Fix bundler version error, slightly better console output

--- canvas_base_box/bashrc_additions.sh
+++ canvas_base_box/bashrc_additions.sh
@@ -1,9 +1,10 @@
 last_commit=$(cd ~/canvas-lms && git log -n 1)
 branch=$(cd ~/canvas-lms && git branch --no-color 2> /dev/null | grep '*' | sed  's/\* //')
 
-echo "
-*******************************
-To run canvas:
+echo -e "
+\e[0m\e[33m*******************************
+To run canvas:\e[0m
+
 > cd ~/canvas-lms
 > foreman start -f Procfile.dev
 
@@ -15,8 +16,8 @@ http://192.168.50.4:3000/
 username: vagrant@example.com
 password: vagrant
 
-*******************************
-Last commit on \"$branch\" branch:
+\e[0m\e[33m*******************************
+Last commit on \"$branch\" branch:\e[0m
 
 $last_commit
 
